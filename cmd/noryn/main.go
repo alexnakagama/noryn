042698685d0db972dfaea622cce3ea1105472a50
alexnakagama <alexnakagama4@gmail.com>
2026-09-17 20:54:40 -0300

    tested on main

--- cmd/noryn/main.go
+++ cmd/noryn/main.go
@@ -1,38 +1,19 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 
-	"github.com/alexnakagama/noryn/internal/agent"
-	"github.com/alexnakagama/noryn/internal/llm"
-	"github.com/alexnakagama/noryn/internal/llm/providers/fake"
+	"github.com/alexnakagama/noryn/internal/tools"
 )
 
 func main() {
-	client := fake.NewClient()
-	agent := agent.New(client)
+	readFile := tools.ReadFileTool{}
 
-	request := llm.Request{
-		Model: "fake",
-		Messages: []llm.Message{
-			{
-				Role:    "user",
-				Content: "Hello",
-			},
-		},
-	}
-
-	response, err := agent.Chat(context.Background(), request)
+	content, err := readFile.Execute(`{"path":"cmd/noryn/main.go"}`)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(response.Message.Content)
-
-	for _, toolCall := range response.ToolCall {
-		fmt.Println("Tool:", toolCall.Name)
-		fmt.Println("Arguments:", toolCall.Arguments)
-	}
+	fmt.Println(content)
 }
