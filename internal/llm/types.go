50e247aec4492cddf47a65f30ec3e791509dca66
alexnakagama <alexnakagama4@gmail.com>
2026-09-17 20:13:34 -0300

    created the structs for the types

--- internal/llm/types.go
+++ internal/llm/types.go
@@ -1,1 +1,22 @@
 package llm
+
+type Message struct {
+	Role    string
+	Message string
+}
+
+type Request struct {
+	Model    string
+	Messages []Message
+}
+
+type Response struct {
+	Message  Message
+	ToolCall []ToolCall
+}
+
+type ToolCall struct {
+	ID        string
+	Name      string
+	Arguments string
+}
