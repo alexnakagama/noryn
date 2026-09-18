54e1c99e22c1ae3cd2849e7029582f3a018197a1
alexnakagama <alexnakagama4@gmail.com>
2026-09-17 22:39:44 -0300

    finished the execute method for the git status tool

--- internal/tools/git_status.go
+++ internal/tools/git_status.go
@@ -1,6 +1,10 @@
 package tools
 
-import "github.com/alexnakagama/noryn/internal/project"
+import (
+	"os/exec"
+
+	"github.com/alexnakagama/noryn/internal/project"
+)
 
 type GitStatusTool struct {
 	project *project.Project
@@ -14,4 +18,16 @@ func NewGitStatusTool(project *project.Project) *GitStatusTool {
 
 func (t *GitStatusTool) Name() string {
 	return "git_status"
+}
+
+func (t *GitStatusTool) Execute(arguements string) (string, error) {
+	cmd := exec.Command("git", "status", "--short")
+	cmd.Dir = t.project.Root
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return string(output), err
+	}
+
+	return string(output), nil
 }
