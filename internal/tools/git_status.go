d798249974843de436cd7dfcc28c6a7279c603b5
alexnakagama <alexnakagama4@gmail.com>
2026-09-18 14:43:37 -0300

    finished the methods for git status

--- internal/tools/git_status.go
+++ internal/tools/git_status.go
@@ -3,6 +3,7 @@ package tools
 import (
 	"os/exec"
 
+	"github.com/alexnakagama/noryn/internal/llm"
 	"github.com/alexnakagama/noryn/internal/project"
 )
 
@@ -18,6 +19,21 @@ func NewGitStatusTool(project *project.Project) *GitStatusTool {
 
 func (t *GitStatusTool) Name() string {
 	return "git_status"
+}
+
+func (t *GitStatusTool) Description() string {
+	return "Show the current Git status of the project."
+}
+
+func (t *GitStatusTool) Definition() llm.ToolDefinition {
+	return llm.ToolDefinition{
+		Name:        t.Name(),
+		Description: t.Description(),
+		Parameters: map[string]any{
+			"type":       "object",
+			"properties": map[string]any{},
+		},
+	}
 }
 
 func (t *GitStatusTool) Execute(arguements string) (string, error) {
