018bce795cf4b1e63d4cf832132257daef50ef05
alexnakagama <alexnakagama4@gmail.com>
2026-09-18 14:28:04 -0300

    added function to the tool interface

--- internal/tools/tool.go
+++ internal/tools/tool.go
@@ -1,6 +1,9 @@
 package tools
 
+import "github.com/alexnakagama/noryn/internal/llm"
+
 type Tool interface {
 	Name() string
 	Execute(arguments string) (string, error)
+	Definition() llm.ToolDefinition
 }
