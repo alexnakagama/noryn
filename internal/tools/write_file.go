ce51a32d766f322e7ce271874c62f4bd9bfbc86d
alexnakagama <alexnakagama4@gmail.com>
2026-09-17 21:24:11 -0300

    finished the method execute

--- internal/tools/write_file.go
+++ internal/tools/write_file.go
@@ -1,5 +1,10 @@
 package tools
 
+import (
+	"encoding/json"
+	"os"
+)
+
 type WriteFileTool struct{}
 
 type writeFileArguments struct {
@@ -11,4 +16,18 @@ func (t *WriteFileTool) Name() string {
 	return "write_file"
 }
 
-func (t *WriteFileTool) Execute(arguments string) (string, error) {}
+func (t *WriteFileTool) Execute(arguments string) (string, error) {
+	var args writeFileArguments
+
+	err := json.Unmarshal([]byte(arguments), &args)
+	if err != nil {
+		return "", err
+	}
+
+	err = os.WriteFile(args.Path, []byte(args.Content), 0644)
+	if err != nil {
+		return "", err
+	}
+
+	return "file written successfully", nil
+}
