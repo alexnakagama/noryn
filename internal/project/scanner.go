7b6745ce5f866c31af1c2ed8adf73583a3515522
alexnakagama <alexnakagama4@gmail.com>
2026-09-17 21:49:19 -0300

    finished the findroot function

--- internal/project/scanner.go
+++ internal/project/scanner.go
@@ -1,3 +1,28 @@
 package project
 
-func FindRoot(start string) (string, error) {}
+import (
+	"errors"
+	"os"
+	"path/filepath"
+)
+
+func FindRoot(start string) (string, error) {
+	current, err := filepath.Abs(start)
+	if err != nil {
+		return "", err
+	}
+
+	for {
+		if _, err := os.Stat(filepath.Join(current, "go.mod")); err == nil {
+			return current, nil
+		}
+
+		parent := filepath.Dir(current)
+
+		if parent == current {
+			return "", errors.New("project root not found")
+		}
+
+		current = parent
+	}
+}
