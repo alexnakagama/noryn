c33b879a85246c2c72b6d13d594cb722848c3379
alexnakagama <alexnakagama4@gmail.com>
2026-09-18 14:13:47 -0300

    finished execute method

--- internal/tools/git_diff.go
+++ internal/tools/git_diff.go
@@ -1,6 +1,11 @@
 package tools
 
-import "github.com/alexnakagama/noryn/internal/project"
+import (
+	"encoding/json"
+	"os/exec"
+
+	"github.com/alexnakagama/noryn/internal/project"
+)
 
 type GitDiffTool struct {
 	project *project.Project
@@ -20,4 +25,32 @@ func (t *GitDiffTool) Name() string {
 	return "git_diff"
 }
 
-func (t *GitDiffTool) Execute(arguments string) (string, error) {}
+func (t *GitDiffTool) Execute(arguments string) (string, error) {
+	var args gitDiffArguments
+
+	err := json.Unmarshal([]byte(arguments), &args)
+	if err != nil {
+		return "", err
+	}
+
+	cmdArgs := []string{"diff"}
+
+	if args.Path != "" {
+		path, err := t.project.ResolvePath(args.Path)
+		if err != nil {
+			return "", err
+		}
+
+		cmdArgs = append(cmdArgs, "--", path)
+	}
+
+	cmd := exec.Command("git", cmdArgs...)
+	cmd.Dir = t.project.Root
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return string(output), err
+	}
+
+	return string(output), nil
+}
