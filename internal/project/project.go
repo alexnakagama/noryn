6d18ce5dc6a9262e344588afc148ff390c1f0b1d
alexnakagama <alexnakagama4@gmail.com>
2026-09-17 21:58:42 -0300

    finished method to discover the path

--- internal/project/project.go
+++ internal/project/project.go
@@ -1,5 +1,28 @@
 package project
 
+import (
+	"fmt"
+	"path/filepath"
+)
+
 type Project struct {
 	Root string
+}
+
+func (p *Project) ResolvePath(path string) (string, error) {
+	absPath, err := filepath.Abs(filepath.Join(p.Root, path))
+	if err != nil {
+		return "", err
+	}
+
+	relPath, err := filepath.Rel(p.Root, absPath)
+	if err != nil {
+		return "", err
+	}
+
+	if relPath == ".." || len(relPath) >= 3 && relPath[:3] == ".."+string(filepath.Separator) {
+		return "", fmt.Errorf("path is outside the project: %s", path)
+	}
+
+	return absPath, nil
 }
