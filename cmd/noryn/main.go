f40cec3d8f4e9d8a1d013d8a93cef4789433046d
alexnakagama <alexnakagama4@gmail.com>
2026-09-18 17:10:49 -0300

    implementation on main and added godotenv to load env

--- cmd/noryn/main.go
+++ cmd/noryn/main.go
@@ -8,9 +8,15 @@ import (
 	"github.com/alexnakagama/noryn/internal/llm/providers/openai"
 	"github.com/alexnakagama/noryn/internal/project"
 	"github.com/alexnakagama/noryn/internal/tools"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+
 	project, err := project.Discover(".")
 	if err != nil {
 		log.Fatal(err)
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/alexnakagama/noryn
 
 go 1.27.1
+
+require github.com/joho/godotenv v1.5.1 // indirect
