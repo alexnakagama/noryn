f2e2e8b21399612f9ebcfd73aa89b073321386c6
alexnakagama <alexnakagama4@gmail.com>
2026-09-18 17:24:18 -0300

    finished the function to load the config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,37 @@
 package config
 
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 type Config struct {
 	Provider string
 	Model    string
 	APIKey   string
 }
 
-func Load() (*Config, error) {}
+func Load() (*Config, error) {
+	if err := godotenv.Load(".env"); err != nil {
+		return nil, err
+	}
+
+	provider := os.Getenv("NORYN_PROVIDER")
+	if provider == "" {
+		provider = "openai"
+	}
+
+	model := os.Getenv("NORYN_MODEL")
+	if model == "" {
+		model = "gpt-5"
+	}
+
+	apiKey := os.Getenv("OPENAI_API_KEY")
+
+	return &Config{
+		Provider: provider,
+		Model:    model,
+		APIKey:   apiKey,
+	}, nil
+}
