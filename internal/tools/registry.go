a78f3186f49a43f39aca3c9e9b408a38a8d51b5d
alexnakagama <alexnakagama4@gmail.com>
2026-09-21 23:54:25 -0300

    finished the constructor and created method to register

--- internal/tools/registry.go
+++ internal/tools/registry.go
@@ -1,8 +1,31 @@
 package tools
 
+import "fmt"
+
 type Registry struct {
 	tools map[string]Tool
 }
 
 func NewRegistry(toolList ...Tool) *Registry {
+	registry := &Registry{
+		tools: make(map[string]Tool),
+	}
+
+	for _, tool := range toolList {
+		registry.Register(tool)
+	}
+
+	return registry
+}
+
+func (r *Registry) Register(tool Tool) error {
+	name := tool.Name()
+
+	if _, exists := r.tools[name]; exists {
+		return fmt.Errorf("tool already registered: %s", name)
+	}
+
+	r.tools[name] = tool
+
+	return nil
 }
