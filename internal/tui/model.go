dfef77aca83a3bb4efc67e24c3049f314aa63156
alexnakagama <alexnakagama4@gmail.com>
2026-09-22 23:12:37 -0300

    fixed imports

--- internal/tui/model.go
+++ internal/tui/model.go
@@ -1,5 +1,11 @@
 package tui
 
+import (
+	"github.com/alexnakagama/noryn/internal/tui/chat"
+	"github.com/alexnakagama/noryn/internal/tui/input"
+	"github.com/alexnakagama/noryn/internal/tui/status"
+)
+
 type Model struct {
 	width  int
 	height int
