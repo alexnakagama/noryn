caebc07d89bda49b4367e8644c134dd9a18f492c
alexnakagama <alexnakagama4@gmail.com>
2026-09-22 23:15:16 -0300

    created new function

--- internal/tui/input/model.go
+++ internal/tui/input/model.go
@@ -5,3 +5,15 @@ import "github.com/charmbracelet/bubbles/textarea"
 type Model struct {
 	textarea textarea.Model
 }
+
+func New() Model {
+	input := textarea.New()
+
+	input.Placeholder = "Ask Noryn anything..."
+	input.Prompt = "> "
+	input.CharLimit = 0
+
+	return Model{
+		textarea: input,
+	}
+}
