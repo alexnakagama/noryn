c3a9ebd5d41d9eb21443cc9bbbca4d7db49329af
alexnakagama <alexnakagama4@gmail.com>
2026-09-22 23:17:06 -0300

    added package to the files missing it and added new functions to the input model

--- internal/tui/input/model.go
+++ internal/tui/input/model.go
@@ -1,6 +1,9 @@
 package input
 
-import "github.com/charmbracelet/bubbles/textarea"
+import (
+	"github.com/charmbracelet/bubbles/textarea"
+	tea "github.com/charmbracelet/bubbletea"
+)
 
 type Model struct {
 	textarea textarea.Model
@@ -16,4 +19,28 @@ func New() Model {
 	return Model{
 		textarea: input,
 	}
+}
+
+func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	m.textarea, cmd = m.textarea.Update(msg)
+
+	return m, cmd
+}
+
+func (m Model) View() string {
+	return m.textarea.View()
+}
+
+func (m Model) Value() string {
+	return m.textarea.Value()
+}
+
+func (m *Model) Reset() {
+	m.textarea.Reset()
+}
+
+func (m *Model) Focus() tea.Cmd {
+	return m.textarea.Focus()
 }
