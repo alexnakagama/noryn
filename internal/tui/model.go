10945750faeb19c276f07ebb3141a8f7a06568ea
alexnakagama <alexnakagama4@gmail.com>
2026-09-23 13:34:34 -0300

    edited the model tui

--- internal/tui/model.go
+++ internal/tui/model.go
@@ -5,6 +5,7 @@ import (
 	"github.com/alexnakagama/noryn/internal/tui/screens/welcome"
 	"github.com/alexnakagama/noryn/internal/tui/styles"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type Model struct {
@@ -57,8 +58,12 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (m Model) View() string {
 	content := m.welcome.View()
 
-	return styles.AppStyle.
-		Width(m.width).
-		Height(m.height).
-		Render(content)
+	return lipgloss.Place(
+		m.width,
+		m.height,
+		lipgloss.Center,
+		lipgloss.Center,
+		content,
+		lipgloss.WithWhitespaceBackground(styles.ColorBase),
+	)
 }
