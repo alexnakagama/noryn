e050bdf4a877cf4affca1391e67bc9e163bc0ab0
alexnakagama <alexnakagama4@gmail.com>
2026-09-23 13:01:16 -0300

    added fmt to the resolve path function to debug

--- internal/project/project.go
+++ internal/project/project.go
@@ -2,6 +2,7 @@ package project
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
 )
 
@@ -10,22 +11,42 @@ type Project struct {
 }
 
 func (p *Project) ResolvePath(path string) (string, error) {
+	fmt.Printf("ROOT: %q\n", p.Root)
+	fmt.Printf("PATH: %q\n", path)
+
 	if path == "" {
 		return "", fmt.Errorf("path cannot be empty")
 	}
 
-	absPath, err := filepath.Abs(filepath.Join(p.Root, path))
+	root, err := filepath.Abs(p.Root)
 	if err != nil {
 		return "", err
 	}
 
-	relPath, err := filepath.Rel(p.Root, absPath)
+	var absPath string
+
+	if filepath.IsAbs(path) {
+		absPath = filepath.Clean(path)
+	} else {
+		absPath, err = filepath.Abs(filepath.Join(root, path))
+		if err != nil {
+			return "", err
+		}
+	}
+
+	relPath, err := filepath.Rel(root, absPath)
 	if err != nil {
 		return "", err
 	}
 
-	if relPath == ".." || len(relPath) >= 3 && relPath[:3] == ".."+string(filepath.Separator) {
+	if relPath == ".." ||
+		len(relPath) >= 3 &&
+			relPath[:3] == ".."+string(filepath.Separator) {
 		return "", fmt.Errorf("path is outside the project: %s", path)
+	}
+
+	if _, err := os.Stat(absPath); err != nil {
+		return "", err
 	}
 
 	return absPath, nil
