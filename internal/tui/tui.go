7e8ab53ef2ecf71178a12391432e22aaac1c1d5b
alexnakagama <alexnakagama4@gmail.com>
2026-09-22 23:29:34 -0300

    added run function

--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -1,1 +1,16 @@
 package tui
+
+import (
+	"github.com/alexnakagama/noryn/internal/agent"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func Run(a *agent.Agent) error {
+	model := New(a)
+
+	program := tea.NewProgram(model)
+
+	_, err := program.Run()
+
+	return err
+}
