dc429c45f3e9fb432773f5afbaf9b51ae071e3e5
alexnakagama <alexnakagama4@gmail.com>
2026-09-22 23:18:09 -0300

    created the model chat

--- internal/tui/chat/model.go
+++ internal/tui/chat/model.go
@@ -1,6 +1,27 @@
 package chat
 
+type Message struct {
+	Role    string
+	Content string
+}
+
 type Model struct {
 	messages  []Message
 	streaming bool
+}
+
+func New() Model {
+	return Model{
+		messages: []Message{},
+	}
+}
+
+func (m Model) AddMessage(message Message) Model {
+	m.messages = append(m.messages, message)
+
+	return m
+}
+
+func (m Model) Messages() []Message {
+	return m.messages
 }
